0294a94810ec53b588f51366b5cf137adc30ab68
LucasRachor <lucas.rachor32@gmail.com>
2025-03-25 19:09:23 -0400

    fix: faltou algumas importacoes hehe

--- src/medicos/medicos.module.ts
+++ src/medicos/medicos.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MedicosService } from './medicos.service';
 import { MedicosController } from './medicos.controller';
 import { PrismaClient } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   controllers: [MedicosController],
-  providers: [MedicosService, PrismaClient],
+  providers: [MedicosService, PrismaClient, UserService],
 })
 export class MedicosModule {}
