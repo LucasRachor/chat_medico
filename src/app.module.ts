9678f4d0b48597ac8981b786811b5ff1fe53853d
LucasRachor <lucas.rachor32@gmail.com>
2025-03-25 15:07:52 -0400

    fix: agora o auth protege a aplicacao inteira

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PacientesModule } from './pacientes/pacientes.module';
 import { MedicosModule } from './medicos/medicos.module';
+import { JwtAuthGuard } from './auth/guards/JwtAuth.guard';
 import { UserModule } from './user/user.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
   imports: [PacientesModule, MedicosModule, UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard
+  }, PrismaClient, JwtService],
 })
-export class AppModule {}
+
+export class AppModule { }
